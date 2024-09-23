1952d3aa28cd5c4dbfebea91c2313e810b76c38b
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-09-23 21:11:04 +0500

    Project Configured

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import './style.css';
+import App from './App.vue';
+
+import Home from '@/pages/Home.vue';
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount('#app');
---
+++ src/pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-red-600">Home Page</div>
+</template>
