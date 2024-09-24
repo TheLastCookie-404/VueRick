dd3191c26345a09380ed2782d4a7e848043aa908
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-09-24 11:10:33 +0500

    Router separated

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import './style.css';
-import App from './App.vue';
+import '@/style.css';
+import App from '@/App.vue';
+import router from '@/router/router';
 
-import Home from '@/pages/Home.vue';
 
-const routes = [
-  {
-    path: "/",
-    component: Home,
-  },
-];
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
 
 createApp(App).use(router).mount('#app');
